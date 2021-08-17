4dd2b666afee54d92ce5dd2ef5f12f1517ceb027
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-08-17 23:53:23 +0100

    commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HNGI8 Task</title>
+</head>
+<body>
+    <a href="/task2/">
+        <h1>Task Two</h1>
+    </a>
+    <a href="/task4/">
+        <h2>Task Four</h2>
+    </a>
+</body>
+</html>
