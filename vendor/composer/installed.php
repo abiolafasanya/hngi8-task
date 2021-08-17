6fe2f0d001b5681233685880086c7e6be9025f6a
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-08-18 00:20:00 +0100

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1d4e70a682e3e1db63a5dc2cb735000973bedc64',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1d4e70a682e3e1db63a5dc2cb735000973bedc64',
+            'dev_requirement' => false,
+        ),
+    ),
+);
