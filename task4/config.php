afe7a65a50bcf6bd187f4dc778164e22d0187682
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-08-18 06:46:50 +0100

    correction

--- task4/config.php
+++ task4/config.php
@@ -10,7 +10,6 @@
 
     $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-     $conn = new mysqli($server, $username, $password, $db);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
