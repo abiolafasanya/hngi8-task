61ea5f4b1d4b917fdb5648112626ee969ed15c53
Abiola Fasanya <hezekiahfasanya@gmail.com>
2021-08-18 01:20:25 +0100

    Update config.php

--- task4/config.php
+++ task4/config.php
@@ -1,9 +1,9 @@
 <?php
     
-    $host ="localhost";
-    $userName ="root";
-    $password = "";
-    $dbName = "contact";
+    $host ="us-cdbr-east-04.cleardb.com";
+    $userName ="b589f91f9e4b53";
+    $password = "1689538f";
+    $dbName = "heroku_5c53743cfadaed2";
 
     $conn = new mysqli($host, $userName, $password, $dbName);
 
