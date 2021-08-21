4e323a6ea1191a7ab81e7579326f3cd4c1f688e8
Abiola Fasanya <hezekiahfasanya@gmail.com>
2021-08-21 11:20:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,13 @@
         <h1>Task Two</h1>
     </a>
     <a href="./task4/">
-        <h1>Task Four</h1>
+        <h1>Task Four with PHP</h1>
+    </a>
+    <a href="https://hngi.herokuapp.com/">
+        <h1>Task Four With Golang</h1>
+    </a>
+       <a href="https://github.com/fastbeetech/hngi8">
+        <h1>Golang Github Repo Link</h1>
     </a>
 </body>
 </html>
